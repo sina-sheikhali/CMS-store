f012620922633fd0c1f927e847cc8eac85bdba45
sina-sh0734 <sinasheikhali.work@gmail.com>
2023-11-20 20:50:01 +0330

    Setting up tailwind and Added font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        grayColor: "#191c24",
+        bodyColor: "#000000",
+        hoverColor: "#0F1015",
+        lightGray: "#6c7293",
+        greenColor: "#00d25b",
+        redColor: "#FC424A",
+        blueColor: "#0090E7",
+        orangeColor: "#FFAB00",
+        purpleColor: "#8F5FE8",
+      },
+    },
   },
   plugins: [],
 };
