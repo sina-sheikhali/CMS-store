b44ccdf359f39e4627152b710e8913a86694799c
sina-sh0734 <sinasheikhali.work@gmail.com>
2023-11-20 22:27:34 +0330

    Developed Sidebar component and installed react-icons

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
-
+import Sidebar from "./Components/Sidebar/Sidebar";
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Sidebar />
+    </div>
+  );
 }
 
 export default App;
